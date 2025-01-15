66d9d3150a197b55ee01ef5d5e931d8c23ade77a
A5harib <asharibhashmi7@gmail.com>
2025-01-16 04:02:31 +0500

    scroll done

---
+++ script.js
@@ -0,0 +1,19 @@
+// const centuries = document.querySelectorAll(".century");
+
+// const observer = new IntersectionObserver(
+//   (entries, observer) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("in-view");
+//       } else {
+//         entry.target.classList.remove("in-view");
+//       }
+//     });
+//   },
+//   {
+//     threshold: 0.5,
+//   }
+// );
+// centuries.forEach((century) => {
+//   observer.observe(century);
+// });
